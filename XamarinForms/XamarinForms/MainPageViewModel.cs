e06a37fc85143c356481c4b7072e26ca2515e806
Malcolm Jack <inquisitorjax@gmail.com>
2017-03-04 11:36:29 +0200

    initial commit - still custom binding compile issue :|

---
+++ XamarinForms/XamarinForms/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Mvvm;
+
+namespace XamarinForms
+{
+    public class MainPageViewModel : BindableBase
+    {
+        private bool _toolbarItem1Visible;
+        private bool _toolbarItem2Visible;
+
+        public MainPageViewModel()
+        {
+            ToolbarItem1Visible = true;
+            ToolbarItem2Visible = true;
+        }
+
+        public bool ToolbarItem1Visible
+        {
+            get { return _toolbarItem1Visible; }
+            set { SetProperty(ref _toolbarItem1Visible, value); }
+        }
+
+        public bool ToolbarItem2Visible
+        {
+            get { return _toolbarItem2Visible; }
+            set { SetProperty(ref _toolbarItem2Visible, value); }
+        }
+    }
+}
---
+++ XamarinForms/XamarinForms/ToolbarItemEx.cs
@@ -0,0 +1,40 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinForms
+{
+    public class ToolbarItemEx : ToolbarItem
+    {
+        //NOTE: Default value is true, because toolbar items are by default visible when added to the Toolbar
+        public static readonly BindableProperty IsVisibleProperty = BindableProperty.Create("IsVisible", typeof(bool), typeof(ToolbarItemEx), true, BindingMode.TwoWay, null, OnIsVisibleChanged);
+
+        public bool IsVisible
+        {
+            get { return (bool)GetValue(IsVisibleProperty); }
+            set { SetValue(IsVisibleProperty, value); }
+        }
+
+        public ContentPage ParentPage
+        {
+            get { return Parent as ContentPage; }
+        }
+
+        private static void OnIsVisibleChanged(BindableObject bindable, bool oldvalue, bool newvalue)
+        {
+            var item = bindable as ToolbarItemEx;
+
+            if (item.ParentPage == null)
+                return;
+
+            var items = item.ParentPage.ToolbarItems;
+
+            if (newvalue && !items.Contains(item))
+            {
+                items.Add(item);
+            }
+            else if (!newvalue && items.Contains(item))
+            {
+                items.Remove(item);
+            }
+        }
+    }
+}
